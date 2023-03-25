bb04835e44f199b29c3a0eb26c49b558601447dd
CreeperLordVader-Iclucia-Ashina <1749065611@qq.com>
2023-03-26 00:29:03 +0800

    Fixing cmake/mx3d-config.cmake

--- cmake/mx3d-config.cmake
+++ cmake/mx3d-config.cmake
@@ -1,5 +1,5 @@
 set(mx3d_FOUND TRUE)
 
-get_filename_component(MX3d_DIR ${MX3d_DIRS} DIRECTORY)
+get_filename_component(MX3d_DIR ${MX3d_DIR} DIRECTORY)
 set(MX3d_INCLUDE_DIRS "${MX3d_DIR}/include")
 include_directories(${MX3d_INCLUDE_DIRS})
