77db1c49cfefc3e3c415137c6b269a95cee67106
CreeperLordVader-Iclucia-Ashina <1749065611@qq.com>
2023-03-29 13:10:12 +0800

    Add include/mx3d_BoundingBox. Add some methods to Vector<T, 3> in include/mx3d_Vector.h

---
+++ include/mx3d_BoudingBox.h
@@ -0,0 +1,14 @@
+#ifndef MX3D_BOUNDINGBOX_H
+#define MX3D_BOUNDINGBOX_H
+
+#include "mx3d_Vector.h"
+
+struct BoundingBox
+{
+    Vec3 lower;
+    Vec3 upper;
+    BoundingBox(const Vec3& _lower, const Vec3& _upper) : lower(_lower), upper(_upper) {}
+    Vec3 size() const { return upper - lower; }
+};
+
+#endif
